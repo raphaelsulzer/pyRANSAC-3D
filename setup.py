664dc6de43582186e49e1aa27daf2acb866f3fb7
raphaelsulzer <raphael.sulzer.1@gmail.com>
2024-04-03 12:03:48 +0200

    changes in setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+    name='pyransac3d',
+    packages=find_packages(include=['pyransac3d']),
+    version='0.1.0',
+    description='pyRANSAC-3D',
+    author='Raphael Sulzer',
+)
